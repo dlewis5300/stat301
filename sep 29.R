214e0cbf8c38a2ed56a0db1a25d1c29cbf2ce02b
dlewis5300 <89417812+dlewis5300@users.noreply.github.com>
2021-11-30 13:28:53 -0500

    Add files via upload

---
+++ sep 29.R
@@ -0,0 +1,6 @@
+# Author: Lewis, Date: Sep 29 2021, Purpose: Calculate the discrete probability for sample data
+
+dbinom(4, size=12, prob=0.2)
+# 0.1328756
+pbinom(4, size=12,prob=0.2)
+[1] 0.9274445
